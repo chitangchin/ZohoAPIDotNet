5863ad2f1c930b31aaf2ffba81985555ecbfbd86
chitangchin <chitangb.chin@gmail.com>
2025-02-21 14:28:06 -0500

    Working user.secrets

--- ZohoAPITests/AccessTokenTest.cs
+++ ZohoAPITests/AccessTokenTest.cs
@@ -4,20 +4,19 @@ using ZohoAPI;
 
 namespace ZohoAPITests
 {
-
     [TestFixture]
     public class AccessTokenIntegrationTests
     {
-        public string? ClientId { get; private set; }
-        public string? ClientSecret { get; private set; }
-        public string? OrgId { get; private set; }
+        private IConfigurationRoot config;
+        public string? ClientId { get; set; }
+        public string? ClientSecret { get; set; }
+        public string? OrgId { get; set; }
 
-        [SetUp]
-        public void Setup()
+        public AccessTokenIntegrationTests()
         {
-            var config = new ConfigurationBuilder()
-                .AddUserSecrets<Program>()
-                .Build();
+            config = new ConfigurationBuilder()
+               .AddUserSecrets<AccessTokenIntegrationTests>()
+               .Build();
 
             ClientId = config["clientId"];
             ClientSecret = config["clientSecret"];
@@ -25,20 +24,14 @@ namespace ZohoAPITests
         }
 
         [Test]
-        public Task GetWithValidCredentials()
+        public void GetWithValidCredentials()
         {
-            if (ClientId == null || ClientSecret == null || OrgId == null)
-            {
-                Environment.Exit(0);
-            }
-
-            var accessToken = new AccessToken(ClientId, ClientSecret, OrgId);
+            var accessToken = new AccessToken(ClientId!, ClientSecret!, OrgId!);
             Assert.DoesNotThrowAsync(async () => await accessToken.Get());
-            return Task.CompletedTask;
         }
 
         [Test]
-        public Task GetWithInvalidCredentials()
+        public void GetWithInvalidCredentials()
         {
             // Should output status 200 ok but with error json
             string invalidId = "invalid";
@@ -48,19 +41,6 @@ namespace ZohoAPITests
             var accessToken = new AccessToken(invalidId, invalidSecret, invalidOrgId);
 
             Assert.ThrowsAsync<Exception>(async () => await accessToken.Get());
-            return Task.CompletedTask;
         }
-        //Not needed -> credentials are required fields, aslong as the three required fields are filled, there cannot be status code 500 even with invalid input
-        //[Test]
-        //public void GetWithMissingFormData()
-        //{
-        //    // Should output status 500 ok but with error json
-        //    var invalidId = "";
-        //    var invalidSecret = "";
-
-        //    var accessToken = new AccessToken(invalidId, invalidSecret);
-
-        //    Assert.ThrowsAsync<Exception>(async () => await accessToken.Get());
-        //}
     }
 }
